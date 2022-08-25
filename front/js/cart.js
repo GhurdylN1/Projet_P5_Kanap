e653e0f379dc6910bece172387ea87e74e6350a5
Charles B <charles.bourree@wastemarketplace.fr>
2022-08-25 13:54:14 +0200

    Fusion data from LS and API

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,65 +1,84 @@
-//fetch("http://localhost:3000/api/products") 
-    //.then((res) => res.json()) 
-    //.then((products) => {
-        //console.log(products)
-        //displayCartProductsList(products);
-      //}) 
-    
+// Ne pas faire de .reload()
 
-//Sauvegarde du panier "cart"
+iniProducts();
+
+let productsWithAllInfos = [];
+
+function iniProducts() {
+    let productsFormLS = getCart();
+    productsFormLS.forEach(productFromLS => {
+        fetch(`http://localhost:3000/api/products/${productFromLS.id}`)
+            .then((res) => res.json())
+            .then((productFromAPI) => {
+                let productWithAllInfos = {};
+                Object.assign(productWithAllInfos, productFromAPI)
+                productWithAllInfos.color = productFromLS.color;
+                delete productWithAllInfos.colors
+                productWithAllInfos.id = productWithAllInfos._id
+                delete productWithAllInfos._id;
+                productsWithAllInfos.push(productWithAllInfos)
+                console.log(productWithAllInfos)
+                displayProduct(productWithAllInfos);
+            })
+    });
+}
+
+// Sauvegarde du panier "cart"
 function saveCart(cart) {
     localStorage.setItem("cart", JSON.stringify(cart));
 }
 
-//fonction d'appel du panier, si il est null retourne un panier vide, sinon retourne le panier sauvegardé au dessus.
+// fonction d'appel du panier, si il est null retourne un panier vide, sinon retourne le panier sauvegardé au dessus.
 function getCart() {
     let cart = localStorage.getItem("cart");
-    if(cart == null){
+    if (cart == null) {
         return [];
-    }else{
+    } else {
         return JSON.parse(cart);
     }
 }
 
-//fonction d'ajout d'un produit au panier et gestion de la quantité et de la couleur
-function addCart(product){
+// fonction d'ajout d'un produit au panier et gestion de la quantité et de la couleur
+function addCart(product) {
     let cart = getCart();
     let foundProduct = cart.find(p => p.id == product.id && p.color == product.color);
-    if(foundProduct != undefined){
+    if (foundProduct != undefined) {
         foundProduct.quantity++;
-    }else{
+    } else {
         product.quantity = 1
         cart.push(product);
     }
     saveCart(cart);
 }
 
-//fonction de retrait d'un produit du panier prenant compte de sa quantité et de sa couleur
-function removeFromCart(product){
+// fonction de retrait d'un produit du panier prenant compte de sa quantité et de sa couleur
+function removeFromCart(product) {
     let cart = getCart();
     cart = cart.filter(p => p.id != product.id && p.color != product.color);
     saveCart(cart);
+    productsWithAllInfos = productsWithAllInfos.filter(p => p.id != product.id && p.color != product.color);
 }
 
-//fonction de modification de quantité prenant compte de sa couleur
-function changeQuantity(product,quantity){
+// fonction de modification de quantité prenant compte de sa couleur
+function changeQuantity(product, newQuantity) {
     let cart = getCart();
-    let foundProduct = cart.find(p => p.id == product.id && p.color == product.color);
-    if(foundProduct != undefined){
-        foundProduct.quantity = quantity;
-        if(foundProduct.quantity <= 0) {
-            removeFromCart(foundProduct);
-        } else {
-            saveCart(cart);
-        }
+    let foundProductFromLS = cart.find(p => p.id == product.id && p.color == product.color);
+    console.log('El to update LS', foundProductFromLS)
+    if (foundProductFromLS != undefined) {
+        foundProductFromLS.quantity = newQuantity;
+        saveCart(cart);
+    }
+    let foundProductWithAllInfos = productsWithAllInfos.find(p => p.id == product.id && p.color == product.color);
+    console.log('El to update all infos', foundProductWithAllInfos)
+    if (foundProductWithAllInfos != undefined) {
+        foundProductWithAllInfos.quantity = newQuantity;
     }
 }
 
 //calcul de quantité de produits dans le panier
 function getNumberProduct() {
-    let cart = getCart();
     let number = 0;
-    for (let product of cart) {
+    for (let product of productsWithAllInfos) {
         number += product.quantity;
     }
     return number;
@@ -67,40 +86,34 @@ function getNumberProduct() {
 
 //calcul du prix total
 function getTotalPrice() {
-    let cart = getCart();
     let total = 0;
-    for (let product of cart) {
+    for (let product of productsWithAllInfos) {
         total += product.quantity * product.price;
     }
     return total;
 
 }
 
-function displayCartProductsList(products) {
+function displayProduct(product) {
     const cartItem = document.querySelector("#cart__items");
-    for (let product of products) {
-        //fetch(`http://localhost:3000/api/products/${product.id}`)
-          //.then((data) => data.json())
-          //.then((product) => {}
+// Création des differents élément html qu'on veut afficher dans le DOM dans une balise "article"
+const article = document.createElement("article");
+article.setAttribute("class", "cart__item");
+article.setAttribute("data-id", `${product.id}`);
+article.setAttribute("data-color", `${product.color}`);
+cartItem.appendChild(article);
 
-            // Création des differents élément html qu'on veut afficher dans le DOM dans une balise "article"
-            const article = document.createElement("article");
-            article.setAttribute("class", "cart__item");
-            article.setAttribute("data-id", `${product.id}`);
-            article.setAttribute("data-color", `${product.color}`);
-            cartItem.appendChild(article);
-            
-            // Image du produit
-            const cartItemImg = document.createElement("div");
-            cartItemImg.setAttribute("class", "cart__item__img");
-            const img = document.createElement("img");
-            img.setAttribute("src", `${product.imageUrl}`);
-            img.setAttribute("alt", `${product.altTxt}`);
-            article.appendChild(cartItemImg);
-            cartItemImg.appendChild(img);
+// Image du produit
+const cartItemImg = document.createElement("div");
+cartItemImg.setAttribute("class", "cart__item__img");
+const img = document.createElement("img");
+img.setAttribute("src", `${product.imageUrl}`);
+img.setAttribute("alt", `${product.altTxt}`);
+article.appendChild(cartItemImg);
+cartItemImg.appendChild(img);
 
-          }
-      }
-    
+// TODO: Autres elements
+}
+
 
 
